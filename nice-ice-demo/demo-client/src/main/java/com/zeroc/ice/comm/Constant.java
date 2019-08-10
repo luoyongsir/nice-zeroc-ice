b1495532f63e68f61a4cc6c6103cbed4e1717b97
luoyong <luoyongsir@mail.com>
2019-08-10 09:55:26 +0800

    添加author

--- nice-ice-demo/demo-client/src/main/java/com/zeroc/ice/comm/Constant.java
+++ nice-ice-demo/demo-client/src/main/java/com/zeroc/ice/comm/Constant.java
@@ -1,10 +1,12 @@
-
 package com.zeroc.ice.comm;
 
+/**
+ * @author Luo Yong
+ */
 public final class Constant {
 
-	public static final String DEMO = "Demo";
+    public static final String DEMO = "Demo";
 
-	private Constant() {
-	}
+    private Constant() {
+    }
 }
